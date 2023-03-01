c602bee197b4d2fbcb4b7ba2a47e4f547ed65abb
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-03-01 13:06:43 +0000

    Bug fix

--- practice.php
+++ practice.php
@@ -11,23 +11,23 @@
 			$arz = array(5, 10, 15, 20, 25);
 			
 			// Mostatil
-			$masahatMostatil = tool[0] * arz[0];
-			$mohitMostatil = tool[0] + arz[0] * 2;
+			$masahatMostatil = $tool[0] * $arz[0];
+			$mohitMostatil = $tool[0] + $arz[0] * 2;
 			echo("Masahat Mostatil : " .$masahatMostatil. "<b>Mohit Mostatil : " .$mohitMostatil);
 
 			echo("<b><b>")
 
 			// Dayere
-			$masahatDayere = tool[1] * 3.14;
-			$mohitDayere = arz[1] * arz[1] * 3.14;
-			echo("Masahat Dayere : " .$masahatDayere. "<b>Mohit Dayere : " .$mohitDayere)
+			$masahatDayere = $tool[1] * 3.14;
+			$mohitDayere = $arz[1] * $arz[1] * 3.14;
+			echo("Masahat Dayere : " .$masahatDayere. "<b>Mohit Dayere : " .$mohitDayere);
 
-			echo("<b><b>")
+			echo("<b><b>");
 
 			// Moraba
-			$masahatMorabaa = tool[2] * tool[2]
-			$mohitMorabaa = tool[2] * 4
-			echo("Masahat Morabaa : " .$masahatMorabaa. "<b>Mohit Morabaa : " .$mohitMorabaa)
+			$masahatMorabaa = $tool[2] * $tool[2];
+			$mohitMorabaa = $tool[2] * 4;
+			echo("Masahat Morabaa : " .$masahatMorabaa. "<b>Mohit Morabaa : " .$mohitMorabaa);
 
 			// Made by Shayan NasrAbadi | 2023 Feb 28 | 19:32
 		?>
